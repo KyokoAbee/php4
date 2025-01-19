b74293c4d58fd3384f313122cb6aea869d41f5bc
KyokoAbee <0406kyoko.k@gmail.com>
2025-01-19 15:16:06 +0900

    ログインしているユーザー名を表示

--- kadai_read.php
+++ kadai_read.php
@@ -10,9 +10,11 @@ check_session_id();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ようこそ</title>
 </head>
 <body>
+<h1>ようこそ <?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8'); ?> さん！</h1>
+
 <a href="kadai_logout.php">logout</a>
 </body>
 </html>
